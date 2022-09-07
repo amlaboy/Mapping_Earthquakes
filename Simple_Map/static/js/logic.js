c4dcfb5e0899b49d75083b021857cc526d25870a
Antonio <amlaboy96@gmail.com>
2022-09-06 22:29:55 -0400

    updated error with the streets map displaying

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -2,12 +2,12 @@
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/1/1/0?access_token=pk.eyJ1IjoiYW1sYWJveSIsImEiOiJjbDdwaG1yOWMxN2QzM3BsaWR2eHp6aDNqIn0.v0U6LmTc_oAPHpcRkhHPcQ', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
+let streets = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
+    maxZoom: 18,
     zoomOffset: -1,
+    id: "mapbox/streets-v11",
     accessToken: API_KEY
 });
 // Then we add our 'graymap' tile layer to the map.
