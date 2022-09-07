138ee2fea6afbe4a0517a700dbdc95948662f320
Antonio <amlaboy96@gmail.com>
2022-09-06 22:38:34 -0400

    added single point code

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -14,4 +14,6 @@ let streets = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
 streets.addTo(map);
 
 //  Add a marker to the map for Los Angeles, California.
-let marker = L.marker([34.0522, -118.2437]).addTo(map);
+let marker = L.circle([34.0522, -118.2437],{
+    radius: 100
+}).addTo(map);
